995fb03585ba1951c058f4a9c574372179a9a7ce
pegah <pegahem20@gmail.com>
2021-02-07 13:14:49 -0800

    Change the transition time

---
+++ app.js
@@ -0,0 +1,19 @@
+function randomRGB(){
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+   return `rgb(${r},${g},${b})`
+}
+
+const letters = document.querySelectorAll('.letter');
+
+// setInterval(function(){
+//     h1.style.color = randomRGB();
+// }, 500)
+
+setInterval(function(){
+    for(let letter of letters) {
+        letter.style.color = randomRGB();
+
+    }
+}, 1000)
